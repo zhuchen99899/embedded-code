a285704dee281135a4cb8ca9077d791d462b5f10
zhuchen99899 <387646983@qq.com>
2020-08-19 10:44:17 +0800

    2020/8/19 修复DMA接收中断被串口1中断打断的bug，调整串口1中断优先级为15，小于串口2中断

--- PID-STM32F103_emwin+freerots/FreeRTOS+EMWIN/SYSTEM/usart/usart.c
+++ PID-STM32F103_emwin+freerots/FreeRTOS+EMWIN/SYSTEM/usart/usart.c
@@ -111,7 +111,7 @@ void uart_init(u32 bound){
 
 	//Usart1 NVIC 配置
 	NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;
-	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority=7 ;//抢占优先级7
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority=15 ;//抢占优先级15
 	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;		//子优先级0
 	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;			//IRQ通道使能
 	NVIC_Init(&NVIC_InitStructure);	//根据指定的参数初始化VIC寄存器
